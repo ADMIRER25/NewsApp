9a1b4ec6a3cd4f7c41d49b1f4f8054679298d3f4
Subha Nayak <subhanayak022@gmail.com>
2024-10-03 15:23:25 +0530

    build changed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active text-white" aria-current="page" to="/">
+                <Link className="nav-link active text-white" aria-current="page" to="/NewsApp">
                   Home
                 </Link>
               </li>
